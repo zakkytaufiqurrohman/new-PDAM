fe7852334889756945a69412d9f901822c8a24f4
taufik <taufik@example.com>
2020-07-12 12:04:22 +0700

    initiate vuex store

---
+++ client/src/store.js
@@ -0,0 +1,59 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    state: {
+        // users: [],
+        // customers: [],
+        // transactions: [],
+        // spends: [],
+    },
+
+    mutations: {
+        // setUsers(state, data) {
+            
+        // },
+
+        // setCustomers(state, data) {
+
+        // },
+
+        // setTransactions(state, data) {
+
+        // },
+
+        // setSpends(state, data) {
+
+        // }
+    },
+
+    actions: {
+        // fetchData(context, args) {
+
+        // },
+
+        // createData(context, args) {
+
+        // },
+
+        // updateData(context, args) {
+
+        // },
+
+        // destroyData(context, args) {
+
+        // },
+
+        // fetchRecord(context, args) {
+
+        // }
+    },
+
+    getters: {
+
+    }
+})
+
+export { store }
