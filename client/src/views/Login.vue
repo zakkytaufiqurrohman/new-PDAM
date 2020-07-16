cfff4f898ac62b49a009e7a2c12ae1af897d851e
taufik <taufik@example.com>
2020-07-17 01:04:01 +0700

    login compoentnn

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -2,42 +2,29 @@
         <div class="row justify-content-center">
             <div class="col-lg-5 col-md-7">
                 <div class="card bg-secondary shadow border-0">
-                    <div class="card-header bg-transparent pb-5">
-                        <div class="text-muted text-center mt-2 mb-3"><small>Sign in with</small></div>
-                        <div class="btn-wrapper text-center">
-                            <a href="#" class="btn btn-neutral btn-icon">
-                                <span class="btn-inner--icon"><img src="img/icons/common/github.svg"></span>
-                                <span class="btn-inner--text">Github</span>
-                            </a>
-                            <a href="#" class="btn btn-neutral btn-icon">
-                                <span class="btn-inner--icon"><img src="img/icons/common/google.svg"></span>
-                                <span class="btn-inner--text">Google</span>
-                            </a>
-                        </div>
-                    </div>
                     <div class="card-body px-lg-5 py-lg-5">
                         <div class="text-center text-muted mb-4">
                             <small>Or sign in with credentials</small>
                         </div>
-                        <form role="form">
+                        <form @submit.prevent="loginForm()" role="form">
                             <base-input class="input-group-alternative mb-3"
                                         placeholder="Email"
                                         addon-left-icon="ni ni-email-83"
-                                        v-model="model.email">
+                                        v-model="form.email">
                             </base-input>
 
                             <base-input class="input-group-alternative"
                                         placeholder="Password"
                                         type="password"
                                         addon-left-icon="ni ni-lock-circle-open"
-                                        v-model="model.password">
+                                        v-model="form.password">
                             </base-input>
 
-                            <base-checkbox class="custom-control-alternative">
+                            <base-checkbox class="custom-control-alternative" v-model="form.remember_me">
                                 <span class="text-muted">Remember me</span>
                             </base-checkbox>
                             <div class="text-center">
-                                <base-button type="primary" class="my-4">Sign in</base-button>
+                                <base-button type="primary" @click.prevent="loginForm()" class="my-4">Sign in</base-button>
                             </div>
                         </form>
                     </div>
@@ -54,16 +41,39 @@
         </div>
 </template>
 <script>
+import { Form } from 'vform'
+import Swal from 'sweetalert2'
+
   export default {
     name: 'login',
     data() {
       return {
-        model: {
+        form: new Form ({
           email: '',
-          password: ''
-        }
+          password: '',
+          remember_me: false
+        })
       }
-    }
+    },
+
+    methods: {
+        loginForm() {
+            this.$store.dispatch('login', this.form).then(() => {
+                Swal.fire(
+                    'Success',
+                    'Login Berhhasil',
+                    'success'
+                )
+                this.$router.push({ path: 'dashboard' })
+            }).catch(() => {
+                Swal.fire(
+                    'Gagal',
+                    'Login Gagal',
+                    'error'
+                )
+            })
+        }
+    },
   }
 </script>
 <style>
