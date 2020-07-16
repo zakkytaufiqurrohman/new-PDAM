7d418afec0736ec6c0233232bc879334ef162690
taufik <taufik@example.com>
2020-07-16 22:04:32 +0700

    add more field in customer form

--- client/src/forms/CustomerForm.vue
+++ client/src/forms/CustomerForm.vue
@@ -1,18 +1,62 @@
 <template>
-    <base-input alternative=""
-        label="Nama Lengkap"
-        placeholder="Nama Lengkap"
-        input-classes="form-control-alternative"
-        v-model="formRecord.name"
-    />
+    <div>
+        <base-input alternative=""
+            label="Nama Lengkap"
+            placeholder="Nama Lengkap"
+            input-classes="form-control-alternative"
+            v-model="formRecord.name"
+        />
+
+        <base-input
+            alternative=""
+            label="No Hp"
+            placeholder="No Hp"
+            input-classes="form-control-alternative"
+            v-model="formRecord.phone"
+        />
+
+        <base-input alternative="" input-classes="form-control-alternative" label="User">
+            <select class="form-control-alternative" v-model="formRecord.user_id">
+                <option value="">Pilih User</option>
+                <option v-for="user in users" :key="user.id" :value="user.id">
+                    {{ user.name }} &ndash; {{ user.email }}
+                </option>
+            </select>
+        </base-input>
+
+        <base-input 
+            alternative=""
+            label="Alamat"
+            input-classes="form-control-alternative"
+            placeholder="Alamat"
+            v-model="formRecord.address"
+        />
+    </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     props: ['formRecord'],
 
-    data: () => ({
+    computed: {
+        users() {
+            return this.$store.getters.users
+        }
+    },
 
-    })
+    methods: {
+        fetchUser() {
+            // this.$store.dispatch('fetchData', 'users')
+            axios.get('http://localhost:8000/api/users').then(res => {
+                this.$store.commit('setUsers', res)
+            })
+        }
+    },
+
+    mounted() {
+        this.fetchUser()
+    }
 }
 </script>
