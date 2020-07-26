120ffd1b8a3b0172e63bd9370e635a9e1e7acaf4
Taufik Rahadi <taufikrahadi1@gmail.com>
2020-07-26 18:29:38 +0700

    transaction crud updated

--- servers/app/Http/Controllers/TransactionController.php
+++ servers/app/Http/Controllers/TransactionController.php
@@ -18,4 +18,67 @@ class TransactionController extends Controller
 
         return TransactionResource::collection($transactions);
     }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'customer_id' => 'required',
+            'user_id' => 'required',
+            'indicator_total' => 'required',
+            'price' => 'required',
+            'status' => 'required'
+        ]);
+
+        $transaction = Transaction::create([
+            'code' => rand(999, 9999),
+            'customer_id' => $request->customer_id,
+            'user_id' => $request->user_id,
+            'indicator_total' => $request->indicator_total,
+            'price' => $request->price,
+            'status' => $request->status,
+        ]);
+
+        if($transaction) {
+            return response([
+                'status' => 'success',
+                'message' => 'transaction created',
+                'data' => $transaction
+            ], 201);
+        } else {
+            return response([
+                'status' => 'failed',
+                'message' => 'error creating transaction',
+            ], 500);
+        }
+    }
+
+    public function update($id, Request $request)
+    {
+        $transaction = Transaction::find($id);
+        $transaction->update([
+            'customer_id' => $request->customer_id,
+            'user_id' => $request->user_id,
+            'indicator_total' => $request->indicator_total,
+            'price' => $request->price,
+            'status' => $request->status,
+        ]);
+        if($transaction) {
+            return response([
+                'status' => 'success',
+                'message' => 'transaction updated',
+                'data' => $transaction
+            ], 200);
+        }
+    }
+
+    public function destroy($id)
+    {
+        $transaction = Transaction::find($id)::delete();
+        if($transaction) {
+            return response([
+                'message' => 'transaction deleted',
+                'status' => 'success'
+            ], 204);
+        }
+    }
 }
